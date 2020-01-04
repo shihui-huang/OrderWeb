391d5bf26e738a7e05bf857fed68fab51f30e915
remi.lamiaux <remi.lamiaux@telecom-sudparis.eu>
2020-01-04 19:14:33 +0100

    use CORS

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 var express = require('express'),
     app = express(),
     port = process.env.PORT || 3000,
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    cors = require('cors');
 
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(cors());
 
 
 var routes = require('./api/routes/router'); //importing route
