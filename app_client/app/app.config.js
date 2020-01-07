64940eb13d8063e80d3ea15a93f5c03a2bdabd08
Arnaud Rouanet <arnaud.rouanet@telecom-sudparis.eu>
2020-01-07 10:04:08 +0100

    Constantes de connexion dans app.config.js

--- app_client/app/app.config.js
+++ app_client/app/app.config.js
@@ -24,3 +24,20 @@ angular.
                 otherwise('/restaurants');
         }
     ]);
+
+var authentificationApp = angular.module('authentificationApp', ['ui.router', 'ui.bootstrap'])
+    /*Les constantes gérant la connexion sont définies ici*/
+    .constant('USER_ROLES', {
+        all : '*',
+        admin : 'admin',
+        user : 'user',
+        owner : 'owner'
+    })
+    .constant('AUTH_EVENTS', {
+        connexionReussie : 'auth-connexion-reussie',
+        connexionEchouee : 'auth-connnexion-echouee',
+        deconnexionReussie : 'auth-deconnexion-reussie',
+        sessionExpiree : 'auth-session-expiree',
+        nonConnecte : 'auth-non-connecte',
+        nonAutorise : 'auth-not-autorise'
+    });
