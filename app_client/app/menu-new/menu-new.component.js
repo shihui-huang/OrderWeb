d8dbc6575e4595e6e88f7cf68c8468a03050e060
remi.lamiaux <remi.lamiaux@telecom-sudparis.eu>
2020-01-07 12:40:06 +0100

    correction de restaurantId undefined pour menu_new

--- app_client/app/menu-new/menu-new.component.js
+++ app_client/app/menu-new/menu-new.component.js
@@ -2,8 +2,8 @@ angular.
     module('menuNew').
     component('menuNew', {
         templateUrl: 'menu-new/menu-new.template.html',
-        controller: ['$http', '$window',
-            function menuNewController($http, $window) {
+        controller: ['$http', '$window', '$routeParams',
+            function menuNewController($http, $window, $routeParams) {
                 var self = this;
 
                 self.doRequest = function() {
@@ -12,14 +12,14 @@ angular.
                     form.description = self.description;
                     form.price = self.price;
                     form.is_available = self.is_available;
-                    form.restaurantId = self.restaurantId;
+                    form.restaurantId = $routeParams.restaurantId;
 
                     var data = JSON.stringify(form);
 
                     $http.post('http://localhost:3000/menus', data).
                         then(function success(response) {
                             self.menu = response.data;
-                            $window.location.href = '#!/restaurants/'+self.restaurantId;
+                            $window.location.href = '#!/restaurants/'+$routeParams.restaurantId;
                         }, function error(response) {
                             console.log(response);
                         }
